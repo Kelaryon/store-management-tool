f328e934333bd698080119da2f58a1e52a1b34e9
Kelatrop <45570401+Kelatrop@users.noreply.github.com>
2026-07-27 14:18:27 +0300

    TASK-03 Additional validation added for signup and login

--- src/main/java/com/kelaryon/store_management_tool/data/LoginRequestDTO.java
+++ src/main/java/com/kelaryon/store_management_tool/data/LoginRequestDTO.java
@@ -1,7 +1,13 @@
 package com.kelaryon.store_management_tool.data;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record LoginRequestDTO(
+        @NotBlank
+        @Email
         String email,
+        @NotBlank
         String password
 ) {
 }
--- src/main/java/com/kelaryon/store_management_tool/data/SignupRequestDTO.java
+++ src/main/java/com/kelaryon/store_management_tool/data/SignupRequestDTO.java
@@ -1,7 +1,17 @@
 package com.kelaryon.store_management_tool.data;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record SignupRequestDTO(
+        @NotBlank
+        @Email
         String email,
+        @Pattern(
+                regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d).{8,}$",
+                message = "Password must contain upper, lower, digit, and be at least 8 characters."
+        )
         String password
 ) {
 }
